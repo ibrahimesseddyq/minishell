4fa3b04413c7f533a4c6f5fd8f1442742d76b630
Youssef Nachat <ynachat@e3r7p18.1337.ma>
2024-07-21 16:18:30 +0100

    multi red v2

--- redirection/ft_redirection.c
+++ redirection/ft_redirection.c
@@ -6,18 +6,21 @@
 /*   By: ynachat <ynachat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 21:21:45 by ynachat           #+#    #+#             */
-/*   Updated: 2024/07/21 15:52:06 by ynachat          ###   ########.fr       */
+/*   Updated: 2024/07/21 16:03:22 by ynachat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 #include "../frontend/frontend.h"
 
-int ft_redirection(t_redir_list *io)
+int ft_redirection(t_redir_list *io, t_astnode *ast)
 {
 	while (io)
 	{
-		if ()
-		io = io->next;  
+		if (io->redir->type == NODE_REDIRECT_OUT || io->redir->type == NODE_REDIRECT_APPEND)
+			ft_red_out(ast);
+		else 
+			ft_red_in(ast);
+		io = io->next;
 	}
 }
