21943c0e3835b3f1e7a2cae72da22ec92b424a3c
Ibrahim Es-seddyq <ibes-sed@e3r2p6.1337.ma>
2024-09-17 13:48:30 +0100

    good

---
+++ expanding/expand_utils1.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_utils1.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibes-sed <ibes-sed@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 12:48:12 by ibes-sed          #+#    #+#             */
+/*   Updated: 2024/09/17 12:48:34 by ibes-sed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include "../frontend/frontend.h"
+
+char	*ft_itoa(int num)
+{
+	char	*str;
+	int		temp_num;
+	int		len;
+	int		sign;
+
+	temp_num = num;
+	len = 1;
+	sign = 1;
+	if (num < 0)
+	{
+		sign = -1;
+		len++;
+	}
+	temp_num = num;
+	while (temp_num != 0)
+	{
+		temp_num /= 10;
+		len++;
+	}
+	str = gcalloc(len + 1);
+	str[len] = '\0';
+	while (len--)
+	{
+		str[len] = (num % 10) * sign + '0';
+		num /= 10;
+	}
+	if (sign == -1)
+		str[0] = '-';
+	return (str);
+}
+
+t_expand_params	init_params(char *line, char *expanded_line)
+{
+	t_expand_params	params;
+
+	params.i = 0;
+	params.expanded_size = 64;
+	params.expanded_index = 0;
+	params.is_inside_quotes = 0;
+	params.current_quote = 0;
+	params.expanded_line = expanded_line;
+	return (params);
+}
+
+void	handle_quotes(char c, t_expand_params *params)
+{
+	if ((c == '\'' || c == '\"') && !params->is_inside_quotes)
+	{
+		params->is_inside_quotes = 1;
+		params->current_quote = c;
+		params->i++;
+	}
+	else if (params->is_inside_quotes && c == params->current_quote)
+	{
+		params->is_inside_quotes = 0;
+		params->current_quote = 0;
+		params->i++;
+	}
+}
+
+void	expand_token(t_expand_params *params, t_lst *env)
+{
+	if (params->expanded_line[params->i] == '$')
+	{
+		params->i++;
+		if (params->expanded_line[params->i] == '?')
+		{
+			expand_exit_status(params);
+		}
+		else
+		{
+			expand_variable(params, env);
+		}
+	}
+	else
+	{
+		append_char(params, params->expanded_line[params->i++]);
+	}
+}
+
+void	expand_exit_status(t_expand_params *params)
+{
+	char	*exit_status_str;
+
+	exit_status_str = ft_itoa(ft_exit(4, GET_EXIT_STATUS));
+	append_string(params, exit_status_str);
+	params->i++;
+}
--- expanding/test_expand.c
+++ expanding/test_expand.c
@@ -1,189 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_expand.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibes-sed <ibes-sed@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 12:19:47 by ibes-sed          #+#    #+#             */
+/*   Updated: 2024/09/17 12:47:03 by ibes-sed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 #include "../frontend/frontend.h"
-#include <stdlib.h>
-#include <string.h>
 
-// Assuming these are defined elsewhere
-
-char *ft_itoa(int num)
+void	expand_variable(t_expand_params *params, t_lst *env)
 {
-    int temp_num = num;
-    int len = 1;
-    int sign = num < 0 ? -1 : 1;
-    if (num < 0) len++; // account for negative sign
-    // Calculate the length of the number
-    while (temp_num /= 10)
-        len++;
-    char *str = gcalloc(len + 1); // +1 for null terminator
-    if (!str) return NULL;
-    str[len] = '\0'; // null terminator
-    while (len--) {
-        str[len] = (num % 10) * sign + '0';
-        num /= 10;
-    }
-    if (sign == -1)
-        str[0] = '-';
-    return str;
+	int		varnamelen;
+	char	*varname;
+	char	*value;
+
+	varnamelen = get_var_length(params->expanded_line, params->i);
+	varname = gcalloc(varnamelen + 1);
+	strncpy(varname, &params->expanded_line[params->i], varnamelen);
+	varname[varnamelen] = '\0';
+	params->i += varnamelen;
+	value = ft_strdup(get_env(env, varname));
+	append_string(params, value);
 }
-char *ft_expand(char *line, t_lst *env)
+
+int	get_var_length(char *line, int i)
 {
-    int is_inside_quotes = 0;
-    char current_quote = 0;
-    int len = ft_strlen(line);
-    char *start = line;
-    int i = 0;
-    int expanded_size = 64;
-    char *expanded_line = gcalloc(expanded_size);
-    if (!expanded_line) return NULL;
-    int expanded_index = 0;
-    while (start[i])
-    {
+	int	len;
 
-        if ((start[i] == '\'' || start[i] == '\"') && !is_inside_quotes)
-        {
-            is_inside_quotes = 1;
-            current_quote = start[i];
-            i++;
-            continue;
-        }
-        else if (is_inside_quotes && start[i] == current_quote)
-        {
-             if (start[i] == '\"' && is_inside_quotes && current_quote == '\"' && start[i - 1] == '$')
-            {
-                        expanded_line[expanded_index++] = '$';
-            }
-            is_inside_quotes = 0;
-            current_quote = 0;
-            i++; 
-            continue;
-        }
-        if (!is_inside_quotes || (is_inside_quotes && current_quote == '\"'))
-        {
-            if(is_inside_quotes && start[i] == ' ')
-            {
-                expanded_line[expanded_index++] = ' ';
-                i++;
+	len = 0;
+	while (line[i + len] && !isspace(line[i + len])
+		&& line[i + len] != '\'' && line[i + len] != '\"'
+		&& line[i + len] != '/' && line[i + len] != '$' && line[i + len] != '=')
+	{
+		len++;
+	}
+	return (len);
+}
 
-            }
-            if (start[i] == '$')
-            {
-                i++;
-                if (start[i] == '?')
-                {
-                    char *exit_status_str = ft_itoa(ft_exit(4, GET_EXIT_STATUS));
-                    if (!exit_status_str)
-                    {
-                        return NULL;
-                    }
-                    for (int j = 0; exit_status_str[j]; j++)
-                    {
-                        if (expanded_index >= expanded_size - 1)
-                        {
-                            expanded_size *= 2;
-                            char *new_expanded_line = realloc(expanded_line, expanded_size);
-                            if (!new_expanded_line)
-                            {
-                                return NULL;
-                            }
-                            expanded_line = new_expanded_line;
-                        }
-                        expanded_line[expanded_index++] = exit_status_str[j];
-                    }
-                    i++;
-                }
-                else if (start[i] == ' ' || start[i] == '\0' || start[i] == '\'' || start[i] == '\"')
-                {
-                    if (start[i] == '\"' || start[i] == '\'' && !is_inside_quotes)
-                    {
-                        continue;
-                    }
-                    else if(start[i] == ' ')
-                    {
-                        expanded_line[expanded_index++] = '$';
-                        expanded_line[expanded_index++] = ' ';
-                    }
-                    else if(is_inside_quotes)
-                    {
-                        expanded_line[expanded_index++] = '$';
-                    }
+void	append_char(t_expand_params *params, char c)
+{
+	if (params->expanded_index >= params->expanded_size - 1)
+	{
+		params->expanded_size *= 2;
+		params->expanded_line
+			= realloc(params->expanded_line, params->expanded_size);
+	}
+	params->expanded_line[params->expanded_index++] = c;
+}
 
-                    i++;
-                }
-                else
-                {
-                    int varNameLen = 0;
-                    while (start[i + varNameLen] && start[i + varNameLen] != ' ' &&
-                           start[i + varNameLen] != '\'' && start[i + varNameLen] != '\"' &&
-                           start[i + varNameLen] != '/' && start[i + varNameLen] != '$' && start[i + varNameLen] != '=')
-                    {
-                        varNameLen++;
-                    }
-                    char *varName = gcalloc(varNameLen + 1);
-                    if (!varName)
-                    {
-                        return NULL;
-                    }
+void	append_string(t_expand_params *params, char *str)
+{
+	int	j;
 
-                    strncpy(varName, &start[i], varNameLen);
-                    varName[varNameLen] = '\0';
-                    i += varNameLen;
-                    char *value2 = ft_strdup(get_env(env, varName));
-                    char *value = value2;
-                    for(int i = 0; value[i] && !is_inside_quotes; i++)
-                    {
-                        if(value[i] == ' ' && !is_inside_quotes)
-                            value[i] = *get_splitted_char(2);
-                    }
-                    if (value)
-                    {
-                        int j = 0;
-                        while (value[j])
-                        {
-                            if (value[j] != '\'' && value[j] != '\"')
-                            {
-                                if (expanded_index >= expanded_size - 1)
-                                {
-                                    expanded_size *= 2;
-                                    char *new_expanded_line = realloc(expanded_line, expanded_size);
-                                    if (!new_expanded_line)
-                                    {
-                                        return NULL;
-                                    }
-                                    expanded_line = new_expanded_line;
-                                }
-                                expanded_line[expanded_index++] = value[j];
-                            }
-                            j++;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                if (expanded_index >= expanded_size - 1)
-                {
-                    expanded_size *= 2;
-                    char *new_expanded_line = realloc(expanded_line, expanded_size);
-                    if (!new_expanded_line)
-                    {
-                        return NULL;
-                    }
-                    expanded_line = new_expanded_line;
-                }
-                expanded_line[expanded_index++] = start[i++];
-            }
-        }
-        else
-        {
-            expanded_line[expanded_index++] = start[i];
-            i++; 
-        }
-    }
+	j = 0;
+	while (str[j])
+	{
+		append_char(params, str[j]);
+		j++;
+	}
+}
 
-    expanded_line[expanded_index] = '\0';
-    return expanded_line;
+char	*ft_expand(char *line, t_lst *env)
+{
+	char			*expanded_line;
+	t_expand_params	params;
+
+	expanded_line = gcalloc(64);
+	params = init_params(line, expanded_line);
+	while (line[params.i])
+	{
+		handle_quotes(line[params.i], &params);
+		if (!params.is_inside_quotes || params.current_quote == '\"')
+		{
+			expand_token(&params, env);
+		}
+		else
+		{
+			append_char(&params, line[params.i++]);
+		}
+	}
+	params.expanded_line[params.expanded_index] = '\0';
+	return (params.expanded_line);
 }
