fb50b2f2cd60a7c1d75994606cff4bc698115c40
Ibrahim Es-seddyq <ibes-sed@e3r3p16.1337.ma>
2024-10-07 18:17:43 +0100

    export test

--- exec/exec_cmd8.c
+++ exec/exec_cmd8.c
@@ -6,35 +6,43 @@
 /*   By: ibes-sed <ibes-sed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 21:46:03 by ibes-sed          #+#    #+#             */
-/*   Updated: 2024/10/06 21:46:56 by ibes-sed         ###   ########.fr       */
+/*   Updated: 2024/10/07 18:16:52 by ibes-sed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static int is_valid_key_export(char c)
+{
+	return (!(
+				('0' <= c && c <= '9')
+				|| ('a' <= c && c <= 'z')
+				|| ('A' <= c && c <= 'Z') || c == '*'
+				|| c == '_' || c == '=' || c == '+' || c== '\"' || c == '\'' || c == '$'
+			));
+}
 int	check_valid2(char *str)
 {
 	int	i;
+	int equal;
 
 	if (!str || !str[0])
 		return (0);
 	i = 0;
+	equal = 0;
 	if (str && (str[0] == '=' || str[0] == '+'))
 		return (0);
 	while (str[i])
 	{
 		if ('0' <= str[i] && str[i] <= '9' && i == 0)
 			return (0);
-		if (
-			!(
-				('0' <= str[i] && str[i] <= '9')
-				|| ('a' <= str[i] && str[i] <= 'z')
-				|| ('A' <= str[i] && str[i] <= 'Z')
-				|| str[i] == '_' || str[i] == '=' || str[i] == '+'
-			)
-		)
+		if (is_valid_key_export(str[i]))
 			return (0);
+		if (str[i] == '=')
+			equal = 1;
 		if (str[i] == '+' && str[i + 1] != '=')
+			return (0);
+		if (str[i] == '*' && equal == 0)
 			return (0);
 		i++;
 	}
