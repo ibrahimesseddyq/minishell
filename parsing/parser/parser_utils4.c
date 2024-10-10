91a4d2e0e328274fc0b32924ffcedc13cf88a1c5
Ibrahim Es-seddyq <ibes-sed@e3r4p9.1337.ma>
2024-10-10 22:00:09 +0100

    fix heredoc leaks

--- parsing/parser/parser_utils4.c
+++ parsing/parser/parser_utils4.c
@@ -6,7 +6,7 @@
 /*   By: ibes-sed <ibes-sed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 16:42:56 by ibes-sed          #+#    #+#             */
-/*   Updated: 2024/10/10 21:43:03 by ibes-sed         ###   ########.fr       */
+/*   Updated: 2024/10/10 21:58:01 by ibes-sed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,17 @@ static int	process_line(char *line, t_heredoc_data *data)
 }
 
 static int	read_and_process_lines(t_heredoc_data *data)
-{
-	char	*line;
+{	char	*line;
 	int		result;
 
 	while (1)
 	{
 		line = readline("> ");
 		if (!ttyname(0))
-			return (handle_tty_input(data->fd));
+			return (free(line), handle_tty_input(data->fd));
 		result = process_line(line, data);
 		if (result != 2)
-			return (result);
+			return (free(line), result);
 		free(line);
 	}
 	return (0);
@@ -58,8 +57,8 @@ int	write_heredoc_to_file(char *delimiter, char *filename, t_lst *env)
 	int				res;
 
 	signal(SIGINT, ft_handler);
-	data.non_expanded_delimiter = delimiter;
-	data.delimiter = ft_expand_delimiter(delimiter);
+	data.non_expanded_delimiter = ft_strdup(delimiter);
+	data.delimiter = ft_expand_delimiter(ft_strdup(delimiter));
 	ft_sprintf(filename, "heredoc_file_%d", file_counter++);
 	file_counter = 1;
 	data.fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
